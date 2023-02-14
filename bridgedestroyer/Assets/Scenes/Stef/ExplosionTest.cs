79ade5fd21a1cd8662f05b5c652326fcc59c2e10
Stef Kluskens <kluskens_stef@hotmail.com>
2023-02-14 12:01:35 +0100

    Kaboom

--- bridgedestroyer/Assets/Scenes/Stef/ExplosionTest.cs
+++ bridgedestroyer/Assets/Scenes/Stef/ExplosionTest.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using RayFire;
 
 public class ExplosionTest : MonoBehaviour
 {
+    [SerializeField] RayfireBomb bomb;
+    [SerializeField] RayfireRigid cube;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -19,5 +23,8 @@ public class ExplosionTest : MonoBehaviour
     public void ExplosionButton()
     {
         Debug.Log("Boom");
+        cube.physics.useGravity = true;
+        bomb.Explode(0);
+        cube.Demolish();
     }
 }
