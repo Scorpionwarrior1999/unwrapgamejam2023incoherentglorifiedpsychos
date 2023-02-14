d5ae1805938d8cf3ce29b040f075e35f23b1897f
SedesDeCock <Decock.sedes@gmail.com>
2023-02-14 12:47:01 +0100

    bombDrag

--- bridgedestroyer/Assets/Scripts/DragBomb.cs
+++ bridgedestroyer/Assets/Scripts/DragBomb.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class DragBomb : MonoBehaviour
+public enum BomType
 {
-    // Start is called before the first frame update
-    void Start()
+    Small, Big, Medium
+
+}
+public class DragBomb : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler
+{
+    [SerializeField]
+    private BomType _type;
+    [SerializeField]
+    private GameObject _bigBomb;
+    [SerializeField]
+    private GameObject _mediumBomb;
+    [SerializeField]
+    private GameObject _smallBomb;
+
+    private GameObject _currentDragItem;
+
+    public void OnBeginDrag(PointerEventData eventData)
     {
-        
+
+        if(_type == BomType.Small)
+        {
+            _currentDragItem =  Instantiate(_smallBomb);
+        }
+        if (_type == BomType.Medium)
+        {
+            _currentDragItem = Instantiate(_mediumBomb);
+        }
+        if (_type == BomType.Big)
+        {
+            _currentDragItem = Instantiate(_bigBomb);
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnDrag(PointerEventData eventData)
     {
-        
+        Vector3 pos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 10);
+        _currentDragItem.transform.position = Camera.main.ScreenToWorldPoint(pos) ;
     }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+
+    }
+
 }
