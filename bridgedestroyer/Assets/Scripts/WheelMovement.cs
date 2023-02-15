53d78f1235d6ac61af2e107340df8ea827070388
JeroenGesquiere <jeroen.gesquiere@student.howest.be>
2023-02-15 14:04:01 +0100

    Wheel Rotation Added

---
+++ bridgedestroyer/Assets/Scripts/WheelMovement.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WheelMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float _rotationSpeed;
+
+    private float _angle;
+
+    void Update()
+    {
+        _angle += Time.deltaTime * _rotationSpeed;
+        transform.rotation = Quaternion.Euler(new Vector3(_angle, 0, 0));
+    }
+}
