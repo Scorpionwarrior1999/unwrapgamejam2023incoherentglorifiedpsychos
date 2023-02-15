1dd891a496899f1c9f32a7b3e74ead0220fbd9d4
Scorpionwarrior1999 <deraes.youri@telenet.be>
2023-02-15 18:10:42 +0100

    added timer lose

--- bridgedestroyer/Assets/Scripts/FailCondition.cs
+++ bridgedestroyer/Assets/Scripts/FailCondition.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class FailCondition : MonoBehaviour
 {
+
+    public float timeLeft;
+
+
+    private void Update()
+    {
+        timeLeft -= Time.deltaTime;
+
+        if (timeLeft <=0)
+        {
+            Debug.Log("you've lost, time over");
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "train")
